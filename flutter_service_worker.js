e61918ed40fab4e42fe3f14394bdeab5326c52de
OusamaAbouKhalil <ousamaaboukhalil@gmail.com>
2026-07-13 05:05:07 +0300

    final

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c34d084b40d956d55fcb65b9e056f607",
+const RESOURCES = {"flutter_bootstrap.js": "bf36603304044b009ea0bfbd8f44f99c",
 "stripe-cancel.html": "41c49cdbf3b43455c213a8171a37798a",
 "version.json": "acb7d84bfcb06ac163f684ef06975f27",
 "splash/background.png": "45d671b7845471acd93020d5477cbf25",
@@ -12,7 +12,7 @@ const RESOURCES = {"flutter_bootstrap.js": "c34d084b40d956d55fcb65b9e056f607",
 "index.html": "15e29033246aa2dbda60ba057aefa777",
 "/": "15e29033246aa2dbda60ba057aefa777",
 "firebase-messaging-sw.js": "c4b2b898d74c2b7d894aa352ac86884d",
-"main.dart.js": "c67287f8d7677c49caf707979d3b6cb5",
+"main.dart.js": "a89462a622921c496674e4cee67a2bba",
 "whish-success.html": "9b2d67715c5c5239cc6633baf88654bb",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "4830f6dc5b6d89855364c619dc02ec55",
